4df4775a7c18966d87f0c80c2c726183c1f95893
HO-Ted <da_rossy@hotmail.com>
2019-12-27 09:00:23 +0000

    Delete retrospectivereadmodel.ts

--- src/app/model/retrospectivereadmodel.ts
+++
@@ -1,9 +0,0 @@
-import { Feedbackmodel } from './feedbackmodel';
-export class Retrospectivereadmodel {
-    Id:number;
-    Name:string;
-    Summary:string;
-    Date:Date;
-    Participants:string;
-    Feedbacks: Feedbackmodel[] = [];
-}
